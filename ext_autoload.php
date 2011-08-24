872d681ac0f5c00c0095541afd14add93fbcf0b0
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-24 09:12:48 +0000

    Corrected case error in autoloader file, resolves #29210
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/datafilter/trunk@51312 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -6,7 +6,7 @@
  */
 $extensionPath = t3lib_extMgm::extPath('datafilter');
 return array(
-	'tx_datafilter_postprocessemptyfiltercheck'	=> $extensionPath . 'interfaces/interface.tx_datafilter_postprocessemptyfiltercheck.php',
+	'tx_datafilter_postprocessemptyfiltercheck'	=> $extensionPath . 'interfaces/interface.tx_datafilter_postprocessEmptyFilterCheck.php',
 	'tx_datafilter_postprocessfilter'	=> $extensionPath . 'interfaces/interface.tx_datafilter_postprocessfilter.php',
 	'tx_datafilter'						=> $extensionPath . 'class.tx_datafilter.php',
 );
