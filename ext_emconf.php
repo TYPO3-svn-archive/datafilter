6eb06280b6e1d393e3b893ccf8c15d85a8efaed3
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-24 09:15:26 +0000

    Released to TER as version 1.2.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/datafilter/trunk@51314 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "datafilter".
 #
-# Auto generated 16-08-2011 08:58
+# Auto generated 24-08-2011 11:14
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.2.0',
+	'version' => '1.2.1',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.3.0-0.0.0',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"015f";s:10:"README.txt";s:4:"3afe";s:23:"class.tx_datafilter.php";s:4:"cb69";s:16:"ext_autoload.php";s:4:"e52f";s:12:"ext_icon.gif";s:4:"bec1";s:17:"ext_localconf.php";s:4:"d3ee";s:14:"ext_tables.php";s:4:"39d1";s:14:"ext_tables.sql";s:4:"972a";s:37:"locallang_csh_txdatafilterfilters.xml";s:4:"7abd";s:16:"locallang_db.xml";s:4:"24c3";s:7:"tca.php";s:4:"0e4d";s:14:"doc/manual.pdf";s:4:"db84";s:14:"doc/manual.sxw";s:4:"99d7";s:14:"doc/manual.txt";s:4:"c347";s:66:"interfaces/interface.tx_datafilter_postprocessEmptyFilterCheck.php";s:4:"3089";s:56:"interfaces/interface.tx_datafilter_postprocessfilter.php";s:4:"05b4";s:35:"res/icons/add_datafilter_wizard.gif";s:4:"8a58";s:40:"res/icons/icon_tx_datafilter_filters.gif";s:4:"dd56";s:42:"tests/tx_datafilter_configuration_Test.php";s:4:"3666";}',
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"97ff";s:10:"README.txt";s:4:"3afe";s:23:"class.tx_datafilter.php";s:4:"cb69";s:16:"ext_autoload.php";s:4:"e6ab";s:12:"ext_icon.gif";s:4:"bec1";s:17:"ext_localconf.php";s:4:"d3ee";s:14:"ext_tables.php";s:4:"39d1";s:14:"ext_tables.sql";s:4:"972a";s:37:"locallang_csh_txdatafilterfilters.xml";s:4:"7abd";s:16:"locallang_db.xml";s:4:"24c3";s:7:"tca.php";s:4:"0e4d";s:14:"doc/manual.pdf";s:4:"db84";s:14:"doc/manual.sxw";s:4:"99d7";s:14:"doc/manual.txt";s:4:"c347";s:66:"interfaces/interface.tx_datafilter_postprocessEmptyFilterCheck.php";s:4:"3089";s:56:"interfaces/interface.tx_datafilter_postprocessfilter.php";s:4:"05b4";s:35:"res/icons/add_datafilter_wizard.gif";s:4:"8a58";s:40:"res/icons/icon_tx_datafilter_filters.gif";s:4:"dd56";s:42:"tests/tx_datafilter_configuration_Test.php";s:4:"3666";}',
 	'suggests' => array(
 	),
 );
