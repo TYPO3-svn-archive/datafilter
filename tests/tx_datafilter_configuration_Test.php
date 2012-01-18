8df39ab377b262d1239ddc8f2b567e5fd9c04552
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-18 19:44:10 +0000

    Reworked test class to use a data provider, references #10066
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/datafilter/trunk@56424 735d13b6-9817-0410-8766-e36946ffe9aa

--- tests/tx_datafilter_configuration_Test.php
+++ tests/tx_datafilter_configuration_Test.php
@@ -37,120 +37,118 @@ class tx_datafilter_configuration_Test extends tx_phpunit_testcase {
 	}
 
 	/**
-	 * Test the simplest possible filter: an equality with a fixed value
+	 * Provides configurations and the expected result for testing filters
 	 *
-	 * @test
+	 * @return array
 	 */
-	public function filterSimple() {
-			// Define the filter to parse
-		$filterDefinition = array(
-			'configuration' => 'tt_content.uid   = 42',
-			'logical_operator' => 'AND'
-		);
-		/**
-		 * @var tx_datafilter	$filterObject
-		 */
-		$filterObject = t3lib_div::makeInstance('tx_datafilter');
-		$filterObject->setData($filterDefinition);
-		$actualResult = $filterObject->getFilterStructure();
-			// Define the expected result
-		$expectedResult = array(
-			'filters' => array(
-				0 => array(
-					'table' => 'tt_content',
-					'field' => 'uid',
-					'main' => FALSE,
-					'void' => FALSE,
-					'conditions' => array(
+	public function configurationProvider() {
+		$configurations = array(
+			'equality' => array(
+				'definition' => array(
+					'configuration' => 'tt_content.uid   = 42',
+					'logical_operator' => 'AND'
+				),
+				'result' => array(
+					'filters' => array(
 						0 => array(
-							'operator' => '=',
-							'value' => '42'
+							'table' => 'tt_content',
+							'field' => 'uid',
+							'main' => FALSE,
+							'void' => FALSE,
+							'conditions' => array(
+								0 => array(
+									'operator' => '=',
+									'value' => '42'
+								)
+							),
+							'string' => 'tt_content.uid   = 42'
 						)
 					),
-					'string' => 'tt_content.uid   = 42'
-				)
+					'logicalOperator' => 'AND',
+					'limit' => array(
+						'max' => 0,
+						'offset' => 0,
+						'pointer' => 0
+					),
+					'orderby' => array(),
+					'parsed' => array(
+						'filters' => array(
+							'tt_content.uid' => array(
+								0 => array(
+									'condition' => '= 42',
+									'operator' => '=',
+									'value' => '42'
+								)
+							)
+						)
+					)
+				),
 			),
-			'logicalOperator' => 'AND',
-			'limit' => array(
-				'max' => 0,
-				'offset' => 0,
-				'pointer' => 0
-			),
-			'orderby' => array(),
-			'parsed' => array(
-				'filters' => array(
-					'tt_content.uid' => array(
+			'array of gp values' => array(
+				'definition' => array(
+					'configuration' => 'tt_content.header like gp:tx_choice',
+					'logical_operator' => 'AND'
+				),
+				'result' => array(
+					'filters' => array(
 						0 => array(
-							'condition' => '= 42',
-							'operator' => '=',
-							'value' => '42'
+							'table' => 'tt_content',
+							'field' => 'header',
+							'main' => FALSE,
+							'void' => FALSE,
+							'conditions' => array(
+								0 => array(
+									'operator' => 'like',
+									'value' => array(
+										'foo',
+										'bar'
+									)
+								)
+							),
+							'string' => 'tt_content.header like gp:tx_choice'
+						)
+					),
+					'logicalOperator' => 'AND',
+					'limit' => array(
+						'max' => 0,
+						'offset' => 0,
+						'pointer' => 0
+					),
+					'orderby' => array(),
+					'parsed' => array(
+						'filters' => array(
+							'tt_content.header' => array(
+								0 => array(
+									'condition' => 'like foo,bar',
+									'operator' => 'like',
+									'value' => 'foo,bar'
+								)
+							)
 						)
 					)
 				)
-			)
+			),
 		);
-			// Check if the "structure" part if correct
-		$this->assertEquals($expectedResult, $actualResult);
+		return $configurations;
 	}
 
 	/**
-	 * Test a filter with array-type values
+	 * Test the parsing various filters
 	 *
+	 * @param string The raw filter definition
+	 * @param array The expected structure of the parsed filter
 	 * @test
+	 * @dataProvider configurationProvider
 	 */
-	public function filterWithArrayValue() {
-			// Define the filter to parse
-		$filterDefinition = array(
-			'configuration' => 'tt_content.header like gp:tx_choice',
-			'logical_operator' => 'AND'
-		);
+	public function testFilters($definition, $result) {
 		/**
 		 * @var tx_datafilter	$filterObject
 		 */
 		$filterObject = t3lib_div::makeInstance('tx_datafilter');
-		$filterObject->setData($filterDefinition);
+		$filterObject->setData($definition);
 		$actualResult = $filterObject->getFilterStructure();
-			// Define the expected result
-		$expectedResult = array(
-			'filters' => array(
-				0 => array(
-					'table' => 'tt_content',
-					'field' => 'header',
-					'main' => FALSE,
-					'void' => FALSE,
-					'conditions' => array(
-						0 => array(
-							'operator' => 'like',
-							'value' => array(
-								'foo',
-								'bar'
-							)
-						)
-					),
-					'string' => 'tt_content.header like gp:tx_choice'
-				)
-			),
-			'logicalOperator' => 'AND',
-			'limit' => array(
-				'max' => 0,
-				'offset' => 0,
-				'pointer' => 0
-			),
-			'orderby' => array(),
-			'parsed' => array(
-				'filters' => array(
-					'tt_content.header' => array(
-						0 => array(
-							'condition' => 'like foo,bar',
-							'operator' => 'like',
-							'value' => 'foo,bar'
-						)
-					)
-				)
-			)
-		);
 			// Check if the "structure" part if correct
-		$this->assertEquals($expectedResult, $actualResult);
+		$this->assertEquals($result, $actualResult);
 	}
 }
 ?>
