5bc6fb810153246eb2423b05abbad74d5f23ed56
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-22 14:40:28 +0000

    Verified compatibility with TYPO3 CMS 6.1, resolves #47518
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/datafilter/trunk@75476 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.6.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '4.5.0-6.1.99',
 			'tesseract' => '1.5.0-0.0.0',
 			'expressions' => '',
 		),
